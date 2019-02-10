84c5f06a16518db35b692b4b7b74c2ede8eb8d72
Hans Christian Reinl <info@drublic.de>
2019-02-10 21:51:31 +0100

    Fix revision

--- revision.php
+++ revision.php
@@ -1,4 +1,4 @@
-<article class="card" id="post-<?php the_ID(); ?>">
+<article class="card" id="post-<?php the_ID(); ?>" data-revision>
 
   <?php if ( is_single() ) : ?>
     <div>
@@ -10,42 +10,43 @@
     >
   <?php endif; ?>
 
-      <?php if ( wd_is_latest_post( get_the_ID() ) ): ?>
-        <span class="cart_tag">Latest</span>
-      <?php endif; ?>
+    <?php if ( wd_is_latest_post( get_the_ID() ) ): ?>
+      <span class="cart_tag">Latest</span>
+    <?php endif; ?>
 
-      <?php if ( is_single() ) : ?>
-        <a href="<?php the_permalink(); ?>"
-          title="<?php the_title(); ?>"
-          rel="bookmark"
-          class="card_link"
-        >
-      <?php endif; ?>
-          <h2 class="text text_type-2 card_title">
-            <?php the_title(); ?>
-          </h2>
-      <?php if ( is_single() ) : ?>
-        </a>
-      <?php endif; ?>
+    <?php if ( is_single() ) : ?>
+      <a href="<?php the_permalink(); ?>"
+        title="<?php the_title(); ?>"
+        rel="bookmark"
+        class="card_link"
+      >
+    <?php endif; ?>
+        <h2 class="text text_type-2 card_title">
+          <?php the_title(); ?>
+        </h2>
+    <?php if ( is_single() ) : ?>
+      </a>
+    <?php endif; ?>
 
-      <p class="text">
-        <span class="text_type-4 card_info_item">
-          <?php the_time('j. F Y'); ?>
-        </span>
+    <p class="text">
+      <span class="text_type-4 card_info_item">
+        <?php the_time('j. F Y'); ?>
+      </span>
 
-        <span class="text_type-4 card_info_item">
-          <?php comments_number('Keine Kommentare', '1 Kommentar', '% Kommentare'); ?>
-        </span>
+      <span class="text_type-4 card_info_item">
+        <?php comments_number('Keine Kommentare', '1 Kommentar', '% Kommentare'); ?>
+      </span>
 
-        <span class="text_type-4 card_info_item">1:01:44h</span>
-      </p>
+      <span class="text_type-4 card_info_item">1:01:44h</span>
+    </p>
+
   <?php if ( is_single() ) : ?>
     </div>
   <?php else: ?>
     </a>
   <?php endif; ?>
 
-  <div class="card_audio" data-revision></div>
+  <div class="card_audio" data-revision-player></div>
 
   <?php if ( !is_single() ) : ?>
     <a href="<?php the_permalink(); ?>" class="card_link text text_type-3">
